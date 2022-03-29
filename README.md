50d42cde221dc7839de025ae4cf2b2226f1b0a94
Boxing <boxingp@gmail.com>
2022-03-29 14:12:34 +0800

    SE-2: get unverified ssl expiry date.

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ This project is to check whether the SSL certification is expired on the target 
 ## Reference:
 
 1. [SSL Expiry](https://github.com/LucasRoesler/ssl-expiry-check)
+2. [Get Unverified SSL Certificate Expiry Date with Python](https://yohanes.gultom.id/2021/10/16/get-unverified-ssl-certificate-expiry-date-with-python/)
--- ssl_expiration_check.py
+++ ssl_expiration_check.py
@@ -3,6 +3,8 @@ import os
 import socket
 import ssl
 
+import OpenSSL
+
 HOSTS_FILE = os.path.join(os.path.dirname(__file__), 'hosts.txt')
 
 
@@ -12,29 +14,26 @@ def get_hosts():
     return hosts
 
 
-def ssl_valid_datetime(hostname: str):
-    ssl_date_fmt = r'%b %d %H:%M:%S %Y %Z'
+def ssl_valid_datetime(hostname: str, port: int = 443):
+    ssl_date_fmt = r'%Y%m%d%H%M%S%z'
 
-    context = ssl.create_default_context()
-    conn = context.wrap_socket(
-        socket.socket(socket.AF_INET),
-        server_hostname=hostname
-    )
-    conn.settimeout(3.0)
-    conn.connect((hostname, 443))
-    ssl_info = conn.getpeercert()
-    start = datetime.datetime.strptime(ssl_info['notBefore'], ssl_date_fmt)
-    expired = datetime.datetime.strptime(ssl_info['notAfter'], ssl_date_fmt)
-    ssl_date = (start, expired)
-    return ssl_date
+    context = ssl.SSLContext()
+    with socket.create_connection((hostname, port)) as conn:
+        with context.wrap_socket(conn, server_hostname=hostname) as sock:
+            certificate = sock.getpeercert(True)
+            cert = ssl.DER_cert_to_PEM_cert(certificate)
+            x509 = OpenSSL.crypto.load_certificate(OpenSSL.crypto.FILETYPE_PEM, cert)
+            cert_starts = datetime.datetime.strptime(x509.get_notBefore().decode('utf-8'), ssl_date_fmt)
+            cert_expires = datetime.datetime.strptime(x509.get_notAfter().decode('utf-8'), ssl_date_fmt)
+            return cert_starts, cert_expires
 
 
 def ssl_valid_time_remaining(hostname: str):
     valid_time = ssl_valid_datetime(hostname)
-    return valid_time[1] - datetime.datetime.utcnow()
+    return valid_time[1] - datetime.datetime.now(datetime.timezone.utc)
 
 
-def test_host(hostname: str, buffer_days: int = 30):
+def test_host(hostname: str, buffer_hours: int = 24, buffer_days: int = 30):
     try:
         will_expire_in = ssl_valid_time_remaining(hostname)
     except ssl.CertificateError as error:
@@ -46,8 +45,10 @@ def test_host(hostname: str, buffer_days: int = 30):
     except socket.gaierror as error:
         return f'{hostname} could not be reached'
     else:
-        if will_expire_in < datetime.timedelta(days=0):
-            return f'{hostname} cert will expired'
+        if will_expire_in <= datetime.timedelta(days=0):
+            return f'{hostname} cert is expired'
+        elif datetime.timedelta(hours=buffer_hours) > will_expire_in > datetime.timedelta(hours=0):
+            return f'{hostname} cert will expired soon in {buffer_hours} hours'
         elif will_expire_in < datetime.timedelta(days=buffer_days):
             return f'{hostname} cert will expire in {will_expire_in}'
         else:
